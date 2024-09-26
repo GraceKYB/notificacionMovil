4c82429d9b73f5d8fae5bf542b6b590b50892bd2
GraceKYB <yaguachi.grace@gmail.com>
2024-09-25 22:31:07 -0500

    notificacion movil

---
+++ Notificacion/Notificacion/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Plugin.LocalNotification;
+using Plugin.LocalNotification.EventArgs;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notificacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            NotificationCenter.Current.NotificationTapped += OnLocalNotificationTapped;
+        }
+        private void OnLocalNotificationTapped(NotificationEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Notificación clickeada");
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await MainPage.Navigation.PushAsync(new HolaPage());
+            });
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Notificacion/Notificacion/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notificacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var Notification = new NotificationRequest
+            {
+                NotificationId=1000,
+                Title="Enviaste un notificación",
+                Description="Acaba de recibir una información desde la app",
+                ReturningData="dato",
+                Schedule= new NotificationRequestSchedule
+                {
+                    NotifyTime=DateTime.Now.AddSeconds(5),
+                }
+            };
+            NotificationCenter.Current.Show(Notification);
+        }
+    }
+}
